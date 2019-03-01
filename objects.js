cd5c439f2bbad4c43af789962711fc45234c77c6
Bright Olayinka <explorebright@gmail.com>
2019-03-01 17:57:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,5 @@
-var playlist = {artist-names: "song titles"};
+var playlist = {artist names: "song titles"};
 
-function updatePlaylist()
+function updatePlaylist(playlist, "artistName", songTitle){
+  return playlist
+}
