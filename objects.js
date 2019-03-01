0393ffb26a0e8656d27d3b342e37d64735394567
Bright Olayinka <explorebright@gmail.com>
2019-03-01 18:24:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,13 +1,3 @@
-var playlist = {artist names: "song titles"};
-
-function updatePlaylist(playlist, "artistName", songTitle){
-  return playlist
-}
-
-function removeFromPlaylist(playlist){
-  delete playlist.song titles;
-  return playlist
-}
 
 var playlist = new Object({keys: '0'});
 
